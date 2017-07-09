5ea6d6b6737f1b4f88b00d1b1f6ab87832bdf779
Zeroji <zzeroji@gmail.com>
2017-07-09 23:37:05 +0200

    Added user relationship getters

--- disband/user.py
+++ disband/user.py
@@ -1,5 +1,6 @@
 from discord.user import *
 from discord.user import User as _User
+from .enums import RelationshipType
 del User
 
 
@@ -10,3 +11,23 @@ class User(_User):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.relationships = {}
+
+    def get_relationship(self, user):
+        """Return a relationship with a user if any, else None."""
+        return self.relationships.get(user.id)
+
+    @property
+    def friends(self):
+        return [r.user for r in self.relationships.values() if r.type == RelationshipType.friend]
+
+    @property
+    def blocked(self):
+        return [r.user for r in self.relationships.values() if r.type == RelationshipType.blocked]
+
+    @property
+    def incoming_requests(self):
+        return [r for r in self.relationships.values() if r.type == RelationshipType.incoming_request]
+
+    @property
+    def outgoing_requests(self):
+        return [r for r in self.relationships.values() if r.type == RelationshipType.outgoing_request]
